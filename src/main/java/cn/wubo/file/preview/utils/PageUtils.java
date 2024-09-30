4534b6367ffda8e484a182b4c9bbca0f84a533e3
吴博 <wubo_aaa@163.com>
2024-09-30 16:38:20 +0800

    新增Free模板渲染方法
    新增Marker了使用FreeMarker模板引擎渲染字符串的方法，该方法接受模板文件名和参数键值对，返回渲染后的字符串。具体功能包括：
    - 创建StringWriter存储渲染结果
    - 配置FreeMarker从类路径加载模板
    - 设置FreeMarker模板版本
    - 处理模板和参数，将结果写入StringWriter
    - 异常情况下抛出自定义PageRuntimeException

--- src/main/java/cn/wubo/file/preview/utils/PageUtils.java
+++ src/main/java/cn/wubo/file/preview/utils/PageUtils.java
@@ -14,6 +14,20 @@ public class PageUtils {
     private PageUtils() {
     }
 
+    /**
+     * 使用FreeMarker模板引擎写入字符串
+     *
+     * @param templateName 模板文件名
+     * @param params 传入模板的参数键值对
+     * @return 渲染后的字符串
+     *
+     * 该方法通过FreeMarker模板引擎，将给定的模板文件和参数渲染成字符串具体功能描述如下：
+     * 1. 创建一个StringWriter对象，用于存储渲染后的字符串
+     * 2. 创建FreeMarker的Configuration对象，并设置模板加载的类路径和版本
+     * 3. 根据模板文件名和字符集获取Template对象
+     * 4. 将参数和Template对象处理后写入StringWriter中
+     * 5. 如果处理过程中出现异常，则捕获后抛出自定义的PageRuntimeException异常
+     */
     public static String write(String templateName, Map<String, Object> params) {
         try (StringWriter sw = new StringWriter()) {
             Configuration cfg = new Configuration(freemarker.template.Configuration.VERSION_2_3_23);
